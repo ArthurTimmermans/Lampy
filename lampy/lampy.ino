ef92f4c19d8343534991be831cf9f5d9f2f6313f
Arthur Timmermans <arthur.timmermans@icloud.com>
2015-11-19 22:46:11 +0100

    Pin i/o setup

--- lampy/lampy.ino
+++ lampy/lampy.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+ *  Lampy
+ *  
+ *  Version: 1.0
+ *  
+ */
 
+void setup() {
+
+  pinMode(2, OUTPUT); //Status led RED
+  pinMode(3, OUTPUT); //Status led YELLOW
+  pinMode(4, OUTPUT); //Status led GREEN
+
+  pinMode(5, INPUT); //InfraRed receiver cable
+
+  pinMode(7, INPUT); //Button 1 (On/Off)
+  pinMode(8, INPUT); //Button 2 (Browse colors)
+
+  pinMode(9, OUTPUT); // RGB RED
+  pinMode(10, OUTPUT); //RGB GREEN
+  pinMode(11, OUTPUT); //RGB BLUE
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
